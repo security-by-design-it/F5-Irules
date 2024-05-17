e7d6ec555a3b72d5f8429d2816fde9e9f80c8501
Jasper Klaren <jasper@sbd-it.nl>
2024-05-17 17:13:16 +0200

    Irule for setting a nonce value on HTML script tags for scipts injected by a ASM Bot Defence Policy

---
+++ set_nonce_js_inject.tcl
@@ -0,0 +1,26 @@
+# IRule to get the nonce value of a CSP header and put it in the HTML script tags
+# This is needed for the JS that is injected by a ASM botdefense profile
+# Make sure to put a HTML profile on the virtual server that triggers an
+# event when it detects a scipt-tag in the HTML.
+
+when HTTP_RESPONSE {
+    # Check if the response header contains a CSP
+    if {[HTTP::header exists "Content-Security-Policy"]} {
+        # Get the CSP header value
+        set csp [HTTP::header value "Content-Security-Policy"]
+        # Check if the CSP contains a nonce
+        if {[string first "nonce-" $csp] != -1} {
+            # Get the nonce value
+            set idx [string first "nonce-" $csp]
+            set nonce [string trim [string range $csp $idx end-3] "nonce-"]
+        }
+    }
+}
+
+# Event if the HTML profile rule is triggered
+when HTML_TAG_MATCHED {
+    # Check if nonce value allready exists, if not add it
+    if {not [HTML::tag attribute exists "nonce"]} {
+        HTML::tag attribute insert "nonce" $nonce
+    }
+}
